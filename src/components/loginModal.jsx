7b8ccbc284b4dc32c1a148361f58a1563dbc2f81
VamshiKrishna4102 <vamshiofficial.4102@gmail.com>
2025-09-03 20:41:10 +0530

    until supbase connection

--- src/components/loginModal.jsx
+++ src/components/loginModal.jsx
@@ -5,40 +5,60 @@ const LoginModal = ({ IsOpen, setIsOpen }) => {
   const [form] = Form.useForm();
   const [formLayout, setFormLayout] = useState("horizontal");
   const [isRegister, setIsRegister] = useState(false);
-  const onFormLayoutChange = ({ layout }) => {};
+  const onSubmitForm = (values) => {
+    console.log("values",values);
+    
+  };
+
   
   return (
     <Modal
       title={` ${isRegister ? "Register" : "Login"} to my-home.in`}
       centered
       open={IsOpen}
-      onOk={() => setIsOpen(false)}
-      okText={ isRegister?"Register Now":"Login Now"}
       onCancel={() => setIsOpen(false)}
+      footer={null}
     >
       <Form
         layout={"vertical"}
         form={form}
         initialValues={{ layout: formLayout }}
-        onValuesChange={onFormLayoutChange}
+        onFinish={onSubmitForm}
         style={{ maxWidth: formLayout === "inline" ? "none" : 600 }}
+        
       >
        {
         isRegister &&
-        <Form.Item label="Full Name">
+        <Form.Item label="Full Name"
+        name="fullname"
+        rules={[{ required: true, message: 'Please input your name!' }]}
+        >
         <Input placeholder="Full Name" />
       </Form.Item>
        }
-        <Form.Item label="Email address">
+        <Form.Item label="Email address"
+         name="email"
+         rules={[{ required: true, message: 'Please input your email address!' },
+          {
+            type:"email",message:"Enter valid email address!"
+          }
+         ]}
+        >
           <Input placeholder="email" />
         </Form.Item>
-        <Form.Item label="Password">
+        <Form.Item label="Password"
+         name="password"
+         rules={[{ required: true, message: 'Please input your password!' }]}
+        >
           <Input placeholder="password here" type="password" />
+        </Form.Item>
+        <Form.Item>
+          <Button  type="primary" htmlType="submit" block>{isRegister?"Register":"Login"} now!</Button>
         </Form.Item>
         <hr />
         <Form.Item>
           {isRegister ? "Have account" : "Don't have account"}
-          <button className="btn btn-link" onClick={() => setIsRegister(!isRegister)}>{isRegister?"Register":"Login"} now!</button>
+          <button className="btn btn-link" onClick={() => setIsRegister(!isRegister)}>{!isRegister?"Register":"Login"} now!</button>
         </Form.Item>
       </Form>
     </Modal>
